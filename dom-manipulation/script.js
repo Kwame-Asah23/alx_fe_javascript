a7f10e754f28beed73543c61fa7211a1caeffa30
Kwame Asah <emmanuelasah1000@gmail.com>
2024-10-21 16:57:07 +0000

    Corrected mistake on the script file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -57,7 +57,7 @@ function addQuote() {
     categoryInput.setAttribute('type', 'text');
     categoryInput.setAttribute('placeholder', 'Enter category');
     submitButton.textContent = 'Add Quote';
-    exportButton.innerHTML = 'Export Quotes';
+    exportButton.textContent = 'Export Quotes';
     importInput.setAttribute('type', 'file');
     importInput.setAttribute('accept', 'application/json');
 
